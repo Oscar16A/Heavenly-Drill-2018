4c3dba1880d1641d240ae58188d5dfd170599996
William Jeon <iamwilly99@gmail.com>
2018-10-19 21:40:55 -0700

    added movement

---
+++ Heavenly-Drill-Game/Assets/Scripts/drillMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drillMovement : MonoBehaviour {
+
+    float speed = .8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-transform.rotation.z*speed, 0, 0, Space.World);
+    }
+}
--- Heavenly-Drill-Game/Assets/Scripts/playerController.cs
+++ Heavenly-Drill-Game/Assets/Scripts/playerController.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+	public float turnspeed = .6f;
+    public float turnbackmodifier = .8f;
+    public float turnlimit = .15f;
+    bool isTurning;
+    public float lBoundary = -5f;
+    public float rBoundary = 5f;
+
 	// Use this for initialization
 	void Start () {
-		
+        isTurning = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("left"))
+		if (Input.GetKey("left") && transform.rotation.z <= turnlimit && transform.position.x > lBoundary)
 		{
-			
-		}
-		if (Input.GetKey("right"))
-		{
+            transform.Rotate(0, 0, turnspeed);
+            isTurning = true;
+        }
 
+		if (Input.GetKey("right") && transform.rotation.z >= -turnlimit && transform.position.x < rBoundary)
+		{
+            transform.Rotate(0, 0, -turnspeed);
+            isTurning = true;
+        }
+        if(transform.rotation.z < .005f && transform.rotation.z > -.005f && !isTurning) {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+		else if(transform.rotation.z < 0f && !isTurning) {
+			transform.Rotate (0, 0, turnspeed * turnbackmodifier);
 		}
+		else if(transform.rotation.z > 0f && !isTurning) {
+			transform.Rotate (0, 0, -turnspeed * turnbackmodifier);
+		}
+        isTurning = false;
 	}
 }
