0bee7c7d17c3d2d1ce01c27034c43d16d2d11273
Oscar16A <oamarill@uci.edu>
2018-10-20 09:42:44 -0700

    scrolling background

---
+++ Heavenly-Drill-Game/Assets/moving_background.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving_background : MonoBehaviour {
+    public Vector3 default_position = new Vector3(0, 0, -5);
+    public Vector3 reset_location = new Vector3(0, 0, -5);
+
+    public float speed = -.2f;
+    public float boost_ratio = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(0, speed*boost_ratio, 0);
+        if (transform.position[1] <= reset_location[1]){
+            transform.position = default_position;
+        }
+
+	}
+}
---
+++ Heavenly-Drill-Game/Assets/moving_item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving_item : MonoBehaviour {
+
+    private Random rnd = new Random();
+    private Vector3 init_pos;
+    public Vector3 speed = ();
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
