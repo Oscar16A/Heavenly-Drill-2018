f6f4010d471ff1b0cb1b37d6b86dad736026b80e
Kevin Touch <ktouch@uci.edu>
2018-10-21 10:30:12 -0700

    Fixed the energy bar

--- Heavenly-Drill-Game/Assets/projectile_script.cs
+++ Heavenly-Drill-Game/Assets/projectile_script.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class projectile_script : MonoBehaviour {
     public float upspeed = 7f;
-<<<<<<< HEAD
     public float lifetime = 3;
-=======
-    public float lifetime = 3.0f;
-
->>>>>>> 75e99a6c2639e08ea1839750a599c488236cb510
 
     private void Start()
     {
