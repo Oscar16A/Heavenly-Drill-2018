dda2f4c82d9d9f2989f92b5caef9a4e1c745df3c
Oscar16A <oamarill@uci.edu>
2018-10-20 18:36:58 -0700

    fixed background

---
+++ Heavenly-Drill-Game/Assets/boost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boost : MonoBehaviour {
+
+    public static float boostRatio;
+    public float boostAmount;
+
+	// Use this for initialization
+	void Start (){
+        boostRatio = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!drillMovement.dead)
+        {
+            if (Input.GetKey("up"))
+            {
+                boostRatio = boostAmount;
+
+            }
+            else
+            {
+                boostRatio = 1f;
+            }
+        }
+        else
+        {
+            boostRatio = 1f;
+        }
+    }
+}
