72ab4f973b23d2b7acd94b653bb2482b48aeb8e7
William Jeon <iamwilly99@gmail.com>
2018-10-21 13:13:58 -0700

    sounds are mostly done

---
+++ Heavenly-Drill-Game/Assets/fastSpawnTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fastSpawnTest : MonoBehaviour {
+
+    public GameObject expla;
+    private float hi = 1f;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= hi)
+        {
+            Instantiate(expla, transform.position, transform.rotation);
+            timer = 0f;
+        }
+    }
+}
